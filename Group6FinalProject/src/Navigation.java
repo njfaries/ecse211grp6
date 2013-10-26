74bb0cbd7c1c9de2ee8348d95d25a2f306d8f6c5
Michael Curtis <michael.curtis@mail.mcgill.ca>
2013-10-26 17:29:37 -0400

    Navigation is delegated to robot position movement.
    Navigation is a timer that will move depending on its moveToX and moveToY attributes.
    Potential changes: Navigation really is just a motor movement rather than an actual navigation controller. Could be renamed PositionMovement and actual logic of navigation is done in a environment reaction class, which changes the moveToX and moveToY attributes as needed.

---
+++ Group6FinalProject/src/Navigation.java
@@ -0,0 +1,47 @@
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.util.TimerListener;
+
+public class Navigation implements TimerListener {
+	
+	private NXTRegulatedMotor leftMotor;
+	private NXTRegulatedMotor rightMotor;
+	private Odometer odometer;
+	//will need getters and setters
+	private double movingToX;
+	private double movingToY;
+	
+	private final double WHEEL_RADIUS = 2.125;
+	private final double WHEEL_BASE = 15.2;
+	private final double DEST_RADIUS = 0.3;
+	private final double MAX_HEADING_ERROR = 5.0;
+	private final double MAX_TURN_TO_ERROR = 1.0;
+	
+	public Navigation(Odometer odometer, NXTRegulatedMotor leftMotor, NXTRegulatedMotor rightMotor) {
+			this.leftMotor = leftMotor;
+			this.rightMotor = rightMotor;
+			this.odometer = odometer;
+	}
+	
+	public void timedOut() {
+	}
+		
+	//executes in while loop fashion outside of timedOut
+	public void travelTo() {
+		
+	}
+		
+	//helper method that returns the calculated required theta in degrees
+	public double requiredHeading(double x, double y) {
+		return 4.5;
+	}
+		
+	//turnTo telling the motors to turn to the correct heading, input in degrees
+	public void turnTo(double theta) {
+		
+	}
+	
+	//helper method to stop motors when last destination is reached
+	public void stopMotors() {
+		
+	}
+}
