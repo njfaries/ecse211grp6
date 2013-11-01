860933cb426ac2e40283ea0b5b1d3163a30e8ef2
nfarie <nfarie@156TR0110DM-07.campus.MCGILL.CA>
2013-11-01 17:12:01 -0400

    Wrote outline of collection code and put in constants for motor rotation.

--- Group6FinalProject/src/robot/collection/CollectionSystem.java
+++ Group6FinalProject/src/robot/collection/CollectionSystem.java
@@ -2,19 +2,22 @@ package robot.collection;
 import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 
-public class CollectionSystem extends Thread {
+public class CollectionSystem extends Thread {	//why is this a thread with no run() method?
 	
-	private NXTRegulatedMotor motor1;
+	private NXTRegulatedMotor cageMotor;
 	private NXTRegulatedMotor motor2;
 	private NXTRegulatedMotor motor3;
 	private NXTRegulatedMotor motor4;
 	
 	public CollectionSystem(NXTRegulatedMotor motor1, NXTRegulatedMotor motor2, NXTRegulatedMotor motor3, NXTRegulatedMotor motor4) {
-		this.motor1 = motor1;
+		cageMotor = motor1;
 		this.motor2 = motor2;
 		this.motor3 = motor3;
 		this.motor4 = motor4;
 	}
+	
+	//cage will start at what position?  This is important...
+	//Motor rotations will be defined to be positive when cage is moving up and negative when cage is moving down.
 	
 	public void collect() {
 		
