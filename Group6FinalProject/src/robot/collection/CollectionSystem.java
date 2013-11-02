9762ad985a005f9b4ebe34cee74f71876a1df761
Andreas Brake <A>
2013-11-02 14:26:38 -0400

    Changed to be specific

--- Group6FinalProject/src/robot/collection/CollectionSystem.java
+++ Group6FinalProject/src/robot/collection/CollectionSystem.java
@@ -27,19 +27,13 @@ public class CollectionSystem extends Thread {	//why is this a thread with no ru
 	
 	//This design operates by first navigating the robot to an appropriate position.  This method is responsible to
 	//lower the cage over the block and loosen to allow the new block to come into the cage.
-	public void collectD1() {
+	public void collect() {
 		//assume cage starts at an angle of 45 degrees
 		//positioning?
 			//robot.navigation.Navigation.turnTo(angle); --> rotate robot so cage is over the discovered block.
 		cageMotor.rotate(-elevate + openClaw);
 		cageMotor.rotate(-openClaw + elevate);
 		//...and that's all, folks.
-	}
-	
-	//This design uses tread system and assumes a feedback system from the cage to confirm that the brick has fallen into the cage
-	public void collectD2() {
-		//while (brickNotInCage) --> some method/sensor feedback here
-		//motors rotate
 	}
 	
 	public void releaseCage() {
