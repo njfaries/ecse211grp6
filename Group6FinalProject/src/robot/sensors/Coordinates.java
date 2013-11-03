8c4b2eb3057dd326fb4781c42d99f4f2301b0943
Michael Curtis <michael.curtis@mail.mcgill.ca>
2013-11-03 13:25:38 -0500

    Moved Coordinates.java to sensor package and deleted testing print methods

---
+++ Group6FinalProject/src/robot/sensors/Coordinates.java
@@ -0,0 +1,143 @@
+import java.lang.Math.*;
+public class Coordinates {
+	
+	//private final int SCOPE_SIZE = 5;
+	//this will take A LOT of fine tuning
+	private final double LATCH_MIDDLE_SLOPE_THRESH = 0.3;
+	private final double OUTSIDE_LATCH_THRESH = 0.5;
+	private boolean latchMiddle = false;
+	private double[] xs;
+	private double[] ys;
+	private double[] slopes;
+	private int leftLatchedIndex;
+	private int rightLatchedIndex;
+	private int middleLatchedIndex;
+	// {m1,b1,m2,b2,m3,b3,m4,b4}
+	private double[] boundaryEquations;
+	private double scannedAtPositionX;
+	private double scannedAtPositionY;
+	
+	//coordinates input in (r,theta) and are constructed to (x,y)
+	public Coordinates(double[] radii, double[] thetas, double x, double y) {
+		this.scannedAtPositionX = x;
+		this.scannedAtPositionY = y;
+		this.xs = radii;
+		this.ys = thetas;
+		convertToXY();
+		this.slopes = generateSlopes();	
+		this.leftLatchedIndex = findLeftIndex();
+		this.rightLatchedIndex = findRightIndex();
+		this.middleLatchedIndex = findMiddleIndex();
+		this.boundaryEquations = generateBoundaryEquations();
+	}
+	
+	//convert to (x,y)
+	public void convertToXY() {
+		double curDist = 0;
+		double curRadians = 0;
+		for(int i = 0; i < xs.length; i++) {
+			curDist = xs[i];
+			curRadians = Math.toRadians(ys[i]);
+			//need to add odo.getX() and odo.getY() HERE!!!!
+			//also take into account robots odo.getTheta() in constructor!!!!
+			xs[i] = scannedAtPositionX + curDist*Math.cos(curRadians);
+			ys[i] = scannedAtPositionY + curDist*Math.sin(curRadians);
+		}
+	}
+	
+	//generate slopes from x and y coordinates
+	public double[] generateSlopes() {
+		int slopesLen = xs.length;
+		double[] slopes = new double[slopesLen - 1];
+		for(int i = 0; i < slopesLen - 1; i++) {
+			slopes[i] = (ys[i+1] - ys[i])/(xs[i+1] - xs[i]);
+		}
+		return slopes;
+	}
+	
+	//method to find left corner block index
+	public int findLeftIndex() {
+		for(int i = 0; i < slopes.length - 1; i++) {
+			if(Math.abs(slopes[i] - slopes[i+1]) > OUTSIDE_LATCH_THRESH) {
+				return i+1;
+			}
+		}
+		//shouldn't get here
+		return 0;
+	}
+	
+	//method to find right corner block index
+	public int findRightIndex() {
+		for(int i = slopes.length - 1; i > 0; i--) {
+			if(Math.abs(slopes[i] - slopes[i-1]) > OUTSIDE_LATCH_THRESH) {
+				return i - 1;
+			}
+		}
+		//shouldn't get here
+		return slopes.length;
+	}
+	
+	//method to find middle corner block index
+	public int findMiddleIndex() {
+		for(int i = leftLatchedIndex; i < rightLatchedIndex - 1; i++) {
+			if(Math.abs(slopes[i+1] - slopes[i]) > LATCH_MIDDLE_SLOPE_THRESH) {
+				latchMiddle = true;
+				return i;
+			}
+		}
+		//shouldn't get here
+		latchMiddle = false;
+		return leftLatchedIndex - rightLatchedIndex;
+	}
+	
+	//method to generate boundary equations based on attributes initiated
+	public double[] generateBoundaryEquations() {
+		//generate two line if latchMiddle is true and one if not
+		if(latchMiddle == true) {
+			double[] es = new double[4];
+			es[0] = findSlope(xs[leftLatchedIndex], ys[leftLatchedIndex], xs[middleLatchedIndex], ys[middleLatchedIndex]);
+			es[1] = findYIntercept(es[0], xs[leftLatchedIndex], ys[leftLatchedIndex]);
+			es[2] = findSlope(xs[middleLatchedIndex], ys[middleLatchedIndex], xs[rightLatchedIndex], ys[rightLatchedIndex]);
+			es[3] = findYIntercept(es[2], xs[rightLatchedIndex], ys[rightLatchedIndex]);
+			//need to generate two more equations based on the two found
+			return es;
+		}
+		else {
+			//if distance between two points is less than thresh we are looking at short side
+		}
+		//tmp for compile
+		double[] tmp = {5,5,5,5};
+		return tmp;
+	}
+	
+	//helper to find fourth vertex
+
+	
+	//helper to return a distance between two points
+	public double longSide(double x1, double y1, double x2, double y2) {
+		return Math.sqrt((x2-x1)*(x2-x1) + (y2 - y1)*(y2 - y1));
+	}
+	
+	//helper to find slope based on four points
+	public double findSlope(double x1, double y1, double x2, double y2) {
+		return (y2 - y1)/(x2 - x1);
+	}
+	
+	//helper to find y-intercept based on a slope and point
+	public double findYIntercept(double m, double x, double y) {
+		return y - m*x;
+	}
+
+	public double[] getBoundaryEquations() {
+		return boundaryEquations;
+	}
+	
+	public double[] getXs() {
+		return xs;
+	}
+	
+	public double[] getYs() {
+		return ys;
+	}
+	
+}
