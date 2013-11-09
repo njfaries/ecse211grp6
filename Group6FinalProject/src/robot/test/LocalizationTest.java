5ab5292564b44b8ff5623320e83e5b8c4b8cfe52
njfaries <njfaries@gmail.com>
2013-11-09 16:24:58 -0500

    Create LocalizationTest.java
    Initial commit of LocalizationTest.java

---
+++ Group6FinalProject/src/robot/test/LocalizationTest.java
@@ -0,0 +1,52 @@
+package robot.test;
+import robot.base.*;
+import robot.localization.*;
+import robot.localization.Localization.Corner;
+import robot.navigation.*;
+import lejos.nxt.ColorSensor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+
+/**
+ * Test class.  Uses mocked classes to test localization
+ * 
+ * @author Nathaniel
+ * @version 1.0.0
+ * @since 2013-11-09
+ *
+ */
+
+public class LocalizationTest {
+	
+	private static NXTRegulatedMotor leftMotor;
+	private static NXTRegulatedMotor rightMotor;
+	private static UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
+	private static ColorSensor csLeft = new ColorSensor(SensorPort.S2);
+	private static ColorSensor csRight = new ColorSensor(SensorPort.S3);
+	private static ColorSensor csBlock = new ColorSensor(SensorPort.S4);
+	
+	private static TwoWheeledRobot robo;
+	private static Localization loc;
+	
+	public static void main(String args[]) {
+		new LocalizationTest();
+	}
+	
+	public LocalizationTest() {
+		robo = new TwoWheeledRobot(leftMotor, rightMotor);
+		loc = new Localization(us, csLeft, csRight, csBlock, Localization.Corner.ONE, robo);
+		
+		new LCDInfo();
+	}
+	
+	public void run() {
+		localize();
+	}
+	
+	public void localize() {
+		loc.start();
+		
+	}
+
+}
