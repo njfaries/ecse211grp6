7f1d8fe48bf6f1489ab966fb1230df5d73a64766
njfaries <njfaries@gmail.com>
2013-11-23 20:11:46 -0500

    Update CollectionSystem.java
    Redesigned collection to used timed movements instead of absolute degrees.  All methods perform the same function as before.

--- Group6FinalProject/src/robot/collection/CollectionSystem.java
+++ Group6FinalProject/src/robot/collection/CollectionSystem.java
@@ -8,12 +8,15 @@ import lejos.nxt.NXTRegulatedMotor;
  * Also deals with releasing the stack after the cage is full.
  * 
  * @author Nathaniel
- * @version 1.1.0
+ * @version 1.2.0
  * @since
  */
 public class CollectionSystem {
-	private static final int elevate = 300; //experimentally determined number of degrees the motor must rotate to reach a 45 degree angle.
-    private static final int openClaw = 200; //experimentally determined number of degrees the motor must rotate to open the claw.
+	
+    private static final int CLAW_SPEED = 150; //speed in degrees per second
+    private static final int CLAW_TIME = 3300; //time in ms
+    private static final int RAISE_TIME = 2000; //time in ms
+    private static final int OPEN_TIME = 1300; //time in ms
     
 	private NXTRegulatedMotor cageMotor;
 	private Navigation nav;
@@ -27,11 +30,8 @@ public class CollectionSystem {
 	public CollectionSystem(NXTRegulatedMotor cageMotor, Navigation nav) {
 		this.cageMotor = cageMotor;
 		this.nav = nav;
+		this.cageMotor.setSpeed(CLAW_SPEED);
 	}
-	
-    //cage will start at what position?  This is important...
-    //Motor rotations will be defined to be positive when cage is moving up and negative when cage is moving down,
-    //and negative when opening the cage and positive when closing the cage.
 	
 	/**
 	 * Initiates the collection of a block (it is assumed that the block has been identified and is therefore fairly close
@@ -39,55 +39,77 @@ public class CollectionSystem {
 	 * 
 	 * @return void
 	 */
-	public void collect(){
-		// If robot needs to be oriented
-		// orientRobot();
-		cageMotor.rotate(openClaw + elevate);
-		cageMotor.rotate(-elevate - openClaw);
-        
+	public void collect() {
+		cageMotor.forward();
+		try {Thread.sleep(CLAW_TIME);} catch(InterruptedException e) {}
+		cageMotor.stop();
+		try {Thread.sleep(CLAW_TIME);} catch(InterruptedException e) {}
+		cageMotor.backward();
+		try {Thread.sleep(CLAW_TIME);} catch(InterruptedException e) {}
+        cageMotor.stop();
 	}
-	// Orients the robot
-	private void orientRobot(){
-		// Orients the robot
-	}
+	
 	/**
 	 * Tells the CollectionSystem to release the cages contents (i.e. opens and backs away)
 	 * To be done at the end of the alloted time to place the stack in the stack zone. (or garbage in the garbage zone)
 	 * @return void
 	 */
-	public void release(){
-		cageMotor.rotate(elevate + openClaw);
+	public void release() {
+		cageMotor.forward();
+		try {Thread.sleep(CLAW_TIME);} catch(InterruptedException e) {}
+		cageMotor.stop();
+		nav.reverse();
+		try {Thread.sleep(2000);} catch(InterruptedException e) {} //arbitrary number
+		nav.stop();
 	}
 		
 	/**
 	 * Lifts the cage (will be called on startup and after identification)
 	 */
-	public void raiseCage(){
-		cageMotor.rotate(-elevate);
+	public void raiseCage() {
+		cageMotor.backward();
+		try {Thread.sleep(RAISE_TIME);} catch(InterruptedException e) {}
+		cageMotor.stop();
 	}
 	/**
 	 * Lowers the cage (will be called before identification)
 	 */
-	public void lowerCage(){
-		cageMotor.rotate(elevate);
+	public void lowerCage() {
+		cageMotor.forward();
+		try {Thread.sleep(RAISE_TIME);} catch(InterruptedException e) {}
+		cageMotor.stop();
 	}
 	/**
 	 * Opens the cage
 	 */
-	public void openCage(){
-		cageMotor.rotate(openClaw);
+	public void openCage() {
+		cageMotor.forward();
+		try {Thread.sleep(OPEN_TIME);} catch(InterruptedException e) {}
+		cageMotor.stop();
 	}
 	/**
 	 * Closes the cage (will be called before identification)
 	 */
-	public void closeCage(){
-		cageMotor.rotate(-openClaw);
+	public void closeCage() {
+		cageMotor.backward();
+		try {Thread.sleep(OPEN_TIME);} catch(InterruptedException e) {}
+		cageMotor.stop();
 	}
 	/**
-	 * Rotates the cage a certain distance
+	 * Rotates the cage a certain distance.  Converts value passed in degrees into an amount of time to rotate.
 	 * @param degrees
 	 */
-	public void rotateCage(int degrees){
-		cageMotor.rotate(degrees);
+	public void rotateCage(int degrees) {
+		int time = (int) ((degrees/150.0) * 1000);	//Explicitly wrote out value for CLAW_TIME to force floating point division.
+		if (time > 0) {
+			cageMotor.forward();
+			try {Thread.sleep(time);} catch(InterruptedException e) {}
+			cageMotor.stop();
+		} else {
+			time = -time;
+			cageMotor.backward();
+			try {Thread.sleep(time);} catch(InterruptedException e) {}
+			cageMotor.stop();
+		}
 	}
 }
